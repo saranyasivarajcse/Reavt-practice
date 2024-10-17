8383abc616d2018dc95e60170e18e4800037ba00
saranya sivaraj <saranya.rajvi@gmail.com>
2024-10-17 10:33:56 +0530

    feat : index and test

---
+++ testapp/functional_component_index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from './App'
+import Test1 from './Test'
+ReactDOM.render(
+<React.StrictMode>
+<Test />
+<Test1 />
+<Test1 />
+<Test1 />
+</React.StrictMode>,
+document.getElementById('root')
+);
---
+++ testapp/functional_component_test.js
@@ -0,0 +1,16 @@
+export function Test1(){
+  return <div>welcome to Test111</div>
+  }
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import Test from './App'
+  import {Test1} from './Test'
+  ReactDOM.render(
+  <React.StrictMode>
+  <Test />
+  <Test1 />
+  <Test1 />
+  <Test1 />
+  </React.StrictMode>,
+  document.getElementById('root')
+  );
