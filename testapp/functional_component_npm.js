593cd0dd61ad807a9e1d5dcd21dabe00e742386f
saranya sivaraj <saranya.rajvi@gmail.com>
2024-10-17 10:45:11 +0530

    feat : functional component js

---
+++ testapp/functional_component_npm.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import TestClass from './TestClass';
+function App() {
+return (
+<div className="App">
+<h1>welcome</h1>
+<TestClass />
+</div>
+);
+}
